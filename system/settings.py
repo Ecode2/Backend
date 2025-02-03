dce8aaf2f95f7f03fbdff503e195c98e5e165651
Ecode2 <ecode5814@gmail.com>
2025-02-03 03:54:55 +0100

    fixed database requrements

--- system/settings.py
+++ system/settings.py
@@ -131,7 +131,7 @@ WSGI_APPLICATION = "system.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if not DEBUG and os.getenv("DATABASE_URL"):
+if DEBUG == False and os.getenv("DATABASE_URL"):
     DATABASES = {
        'default': dj_database_url.config(default=os.getenv("DATABASE_URL"),
                                           conn_max_age=600,
