11ba2cd59e451b5b25d08fd5047464185a948e0a
Ecode2 <ecode5814@gmail.com>
2025-02-03 03:16:05 +0100

    fixed cors issue

--- system/settings.py
+++ system/settings.py
@@ -59,10 +59,10 @@ if os.getenv("DEBUG", default=False) == False:
 else:
     DEBUG = True
 
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ["*"]
 
-    #CORS_ALLOW_ALL_ORIGINS = True
-    CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
+    CORS_ALLOW_ALL_ORIGINS = True
+    #CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000", "http://172.20.10.5:3000"]
 
     #CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
 
